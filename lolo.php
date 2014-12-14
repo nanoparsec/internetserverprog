41e09b30dc630beb554e2422d058c79120931025
nerdbabe <mail@nerdbabe.de>
2014-12-15 00:16:35 +0100

    Revert "deleted files"
    This reverts commit 0897c618a12621b23570ee60eb35f63897251bca.

---
+++ lolo.php
@@ -0,0 +1,11 @@
+<?php
+$wunsch1 = $_POST["wunsch1"];
+$wunsch2 = $_POST["wunsch2"];
+$wunsch3 = $_POST["wunsch3"];
+
+?>
+<ol>
+  <li><?php echo $_POST["wunsch1"]; ?></li>
+  <li><?php echo $_POST["wunsch2"] ?></li>
+  <li><?php echo $_POST["wunsch3"] ?></li>
+</ol>
